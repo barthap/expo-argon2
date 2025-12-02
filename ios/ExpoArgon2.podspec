c86077bbd21513d9044030c5ade18e896b90903b
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-12-02 14:39:56 +0100

    Implement iOS

--- ios/ExpoArgon2.podspec
+++ ios/ExpoArgon2.podspec
@@ -19,6 +19,7 @@ Pod::Spec.new do |s|
   s.static_framework = true
 
   s.dependency 'ExpoModulesCore'
+  s.dependency 'Argon2Swift', '~> 1.0'
 
   # Swift/Objective-C compatibility
   s.pod_target_xcconfig = {
--- ios/ExpoArgon2Module.swift
+++ ios/ExpoArgon2Module.swift
@@ -1,48 +1,117 @@
 import ExpoModulesCore
+import Argon2Swift
 
-public class ExpoArgon2Module: Module {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
-  public func definition() -> ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('ExpoArgon2')` in JavaScript.
-    Name("ExpoArgon2")
+enum SaltEncoding: String, Enumerable {
+    case hex
+    case utf8
+}
 
-    // Defines constant property on the module.
-    Constant("PI") {
-      Double.pi
-    }
-
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      return "Hello world! 👋"
-    }
-
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { (value: String) in
-      // Send an event to JavaScript.
-      self.sendEvent("onChange", [
-        "value": value
-      ])
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of the
-    // view definition: Prop, Events.
-    View(ExpoArgon2View.self) {
-      // Defines a setter for the `url` prop.
-      Prop("url") { (view: ExpoArgon2View, url: URL) in
-        if view.webView.url != url {
-          view.webView.load(URLRequest(url: url))
+enum Argon2Mode: String, Enumerable {
+    case argon2i
+    case argon2d
+    case argon2id
+    
+    func getArgon2Mode() -> Argon2Type {
+        switch self {
+        case .argon2d:
+          return .d
+        case .argon2i:
+          return .i
+        case .argon2id:
+          return .id
         }
       }
+}
 
-      Events("onLoad")
-    }
+struct Argon2Config: Record {
+    @Field
+    var iterations: Int = 2
+    
+    @Field
+    var memory: Int = 32 * 1024
+    
+    @Field
+    var parallelism: Int = 1
+    
+    @Field
+    var hashLength: Int = 32
+    
+    @Field
+    var saltEncoding: SaltEncoding = .utf8
+    
+    @Field
+    var mode: Argon2Mode = .argon2id
+}
+
+public class ExpoArgon2Module: Module {
+  public func definition() -> ModuleDefinition {
+    Name("ExpoArgon2")
+
+      AsyncFunction("hashStringAsync") { (password: String, salt: String, config: Argon2Config) in
+          let saltData: Data? = switch config.saltEncoding {
+          case .hex: Data(hexEncoded: salt)
+          case .utf8: salt.data(using: .utf8)
+          }
+          
+          guard let validSaltData = saltData else {
+              throw InvalidSaltFormatException()
+          }
+          let saltObject = Salt(bytes: validSaltData)
+          
+          do {
+              // Generate hash using Argon2Swift
+              let result = try Argon2Swift.hashPasswordString(
+                password: password,
+                salt: saltObject,
+                iterations: config.iterations,
+                memory: config.memory,
+                parallelism: config.parallelism,
+                length: config.hashLength,
+                type: config.mode.getArgon2Mode(),
+              )
+              
+              // Get encoded and raw hash values
+              let encodedHash = result.encodedString()
+              let rawHash = result.hexString()
+              
+              let resultDictionary = [
+                "rawHash": rawHash,
+                "encodedHash": encodedHash,
+              ]
+              return resultDictionary
+              
+          } catch {
+              throw Argon2FailedException(error.localizedDescription)
+          }
+      }
   }
+}
+
+extension Data {
+    init?(hexEncoded: String) {
+        let hex = hexEncoded.replacingOccurrences(of: "0x", with: "")
+        guard hex.count.isMultiple(of: 2) else {
+            return nil
+        }
+        
+        let chars = hex.map { $0 }
+        let bytes = stride(from: 0, to: chars.count, by: 2)
+            .map { String(chars[$0]) + String(chars[$0 + 1]) }
+            .compactMap { UInt8($0, radix: 16) }
+        
+        guard hex.count / bytes.count == 2 else { return nil }
+        self.init(bytes)
+    }
+}
+
+final class InvalidSaltFormatException : Exception, @unchecked Sendable {
+    override var reason: String {
+        "Invalid salt format"
+    }
+}
+
+final class Argon2FailedException : GenericException<String>, @unchecked Sendable {
+    override var reason: String {
+        "Failed to generate Argon2 hash: \(param)"
+    }
 }
